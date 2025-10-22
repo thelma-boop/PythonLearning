76878eafe5b789de58dc5505797f31778d642413
thelma-boop <thelma.working@gmail.com>
2025-10-22 17:11:24 +0800

    Update and rename Ex08.py to Ex08_List_2D.py
    新增程式大綱：二維串列

--- 20250810Python/Ex08_List_2D.py
+++ 20250810Python/Ex08_List_2D.py
@@ -1,26 +1,43 @@
+'''
+程式大綱：二維串列
+'''
 
 students = [ [90,80,70,60] , [99,88,77,99] , [60,70,80]  ]
 #              0  1  2  3      0  1  2  3      0  1  2    索引值
 #                 0                 1             2       索引值
 
-stu = [10,20,30,40]
+print(students[2])        # [60,70,80]
 
-print(students[2])   #[60,70,80]
-
-print(students[1][2]) # 77
+print(students[1][2])     # 77
 
 
 for i in students:
     print(i)
-    
 
-print()
+# [90,80,70,60]
+# [99,99,77,99]
+# [60,70,80]
+    
+print()    # 分隔線
 
 for i in students:
     for x in i:
         print(x)
 
+# 90
+# 80
+# 70
+# 60
+# 99
+# 88
+# 77
+# 99
+# 60
+# 70
+# 80
+
 
     
     
+
 
