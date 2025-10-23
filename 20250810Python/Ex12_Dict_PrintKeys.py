474e796ca138dbf0e32e3da89c9d81cd74f77cb8
thelma-boop <thelma.working@gmail.com>
2025-10-23 16:33:22 +0800

    Update and rename Ex12.py to Ex12_Dict_PrintKeys.py
    新增程式大綱：印出 Dictionary 的 Keys

--- 20250810Python/Ex12_Dict_PrintKeys.py
+++ 20250810Python/Ex12_Dict_PrintKeys.py
@@ -1,7 +1,11 @@
+'''
+程式大綱：印出 Dictionary 的 Keys
+'''
+
 device = {'light':1,'temperature':26,'door':1}
 
 for item in device:
-    print(item)
+    print(item)        # 印出 device 的 Keys：light, temperature, door
     
     if item == 'light':
         if device[item] == 0:
@@ -11,4 +15,5 @@ for item in device:
             
             
             
+
     
