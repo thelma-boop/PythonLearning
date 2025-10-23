be4ec31de360196cf22fc31b7e0441c60f486f11
thelma-boop <thelma.working@gmail.com>
2025-10-23 16:23:31 +0800

    Update and rename Ex11.py to Ex11_Dict_ScoreInput.py
    新增程式大綱：學生成績輸入系統

--- 20250810Python/Ex11_Dict_ScoreInput.py
+++ 20250810Python/Ex11_Dict_ScoreInput.py
@@ -1,3 +1,8 @@
+'''
+程式大綱：
+使用 Dictionary 製作「學生成績輸入系統」
+'''
+
 print('學生成績輸入系統')
 students = {}
 
@@ -5,14 +10,17 @@ while True:
     name = input('學生姓名(q離開)：')
     if name == 'q':
         break
-    
+
+    ## 若 USER 輸入的學生姓名在 students 裡，則印出該學生姓名及成績
     if name in students:
         print(name,'分數：',students[name])
-        
+
+    ## 若 USER 輸入的學生姓名不在 students 裡，則新增該學生姓名及成績至 students
     else:
         score = int(input('分數：'))
         students[name] = score
         
         
 print(students)        
+
     
