3f5bbaad5e29f225448848881ce1159e2d8e69d1
thelma-boop <thelma.working@gmail.com>
2025-10-28 17:29:46 +0800

    Update Ex16.py
    註解補充

--- 20250810Python/Ex16.py
+++ 20250810Python/Ex16.py
@@ -1,3 +1,7 @@
+'''
+程式大綱
+'''
+
 students = {'Peter':50,'Bill':95,'Mary':89,'Sue':56,'Eric':88,'蘋果':30}
         #     key :value           k    v
         
@@ -5,29 +9,29 @@ students = {'Peter':50,'Bill':95,'Mary':89,'Sue':56,'Eric':88,'蘋果':30}
 # students() => 函式
 #  [] => 取值
 data = [100,200,300,400]
-print(data[1])
+print(data[1])                # 200
 
-print(students['Mary'])
+print(students['Mary'])       # 89
 print('-'*50)
 
         
 stu1 = students['Sue']  # 只有一個值
 price = students['蘋果']
 
-print(stu1)
-print(price)
+print(stu1)                   # 56
+print(price)                  # 30
 
 allstu = students.items()
-print(allstu)
-liststu = list(allstu) # 轉換成串列
-print(liststu)
-print(liststu[0])
-print(liststu[0][0])
-print(liststu[0][1])
+print(allstu)                 # dict_items([('Peter', 50), ('Bill', 95), ('Mary', 89), ('Sue', 56), ('Eric', 88), ('蘋果', 30)])
+liststu = list(allstu)        # 轉換成串列
+print(liststu)                # [('Peter', 50), ('Bill', 95), ('Mary', 89), ('Sue', 56), ('Eric', 88), ('蘋果', 30)]
+print(liststu[0])             # ('Peter', 50)
+print(liststu[0][0])          # Peter
+print(liststu[0][1])          # 50
 
 #   [  ('Peter', 50)  ,  ('Bill', 95), ('Mary', 89), ('Sue', 56), ('Eric', 88), ('蘋果', 30)  ]
-#      0     1           0     1      0      1      0     1
-#        0                1             2               3            4            5
+#          0     1           0     1      0      1      0    1        0    1        0    1
+#             0                1             2             3            4            5
 print(liststu[4][0])
 print(liststu[4][1])
 print('-'*50)
@@ -45,6 +49,7 @@ print('-'*50)
 for k,v in students.items():  #會得到每一組資料 (key,value)二個內容 ，就可以用二個變數分別來給予
     print(k)
     print(v)
+
 
 
 
