d4474c2417ae75d4bd21044b3fdc0021c7ccc0d2
thelma-boop <thelma.working@gmail.com>
2025-10-30 15:03:05 +0800

    Update Example03_Datetime_RegisterSystem.py
    新增程式大綱：datetime, 範例：報到系統

--- 20250817Python/Example03_Datetime_RegisterSystem.py
+++ 20250817Python/Example03_Datetime_RegisterSystem.py
@@ -1,22 +1,28 @@
+'''
+程式大綱：
+1) datetime 函式庫
+2) datetime to string：strftime()
+3) Example：會議報到系統
+'''
+
 from datetime import datetime
 ## 正規表達式 import re
 
 current_Time = datetime.now()		# 抓取目前的時間
-print(current_Time)                 # 2025-10-29 07:17:07.756948
+print(current_Time)                 # <class 'datetime.datetime'> 2025-10-29 07:17:07.756948
 
 today = datetime.today()			# 抓取目前的時間
-print(today)                        # 2025-10-29 07:17:07.756979
+print(today)                        # <class 'datetime.datetime'> 2025-10-29 07:17:07.756979
 
 ##-----將時間轉字串 (f = 格式化)-----
-date_only = today.strftime("%Y-%m-%d")		# yyyy-mm-dd (%y == yy)
-
-print(date_only)
+date_only = today.strftime("%Y-%m-%d")    # yyyy-mm-dd (%y == yy)
+print(date_only)                          # "2025-10-30"
 
 time_only = today.strftime("%H:%M:%S")
-print(time_only)
+print(time_only)                          # "06:48:00"
 
 regist_time = today.strftime("%Y/%m/%d %H:%M:%S")
-print(regist_time)
+print(regist_time)                        # 2025/10/30 06:48:00
 
 
 ##-----範例：會議報到系統-----
@@ -41,9 +47,17 @@ while name != 'q' :
         today = datetime.today()
         regist = today.strftime("%Y/%m/%d %H:%M:%S")
         people[name] = regist
-        print(name, '在', regist, '報到')
+        print(name, '在', regist, '報到')        # Alice 在 2025/10/30 06:55:30 報到
        
 ## f：格式化；在字串前加 f 表示裡面會有格式化參數
 print(f'共有{len(people)}人，為：{people}')
-        
+
+'''
+共有5人，為：{'Alice': '2025/10/30 06:55:30', 
+             'Bob': '2025/10/30 06:55:46', 
+             'Catheline': '2025/10/30 06:55:57', 
+             'Diego': '2025/10/30 06:56:07', 
+             'Ema': '2025/10/30 06:56:19'}
+'''     
+
 
